b0e6e877126a716b59464ee805a83da7e85c1198
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-18 00:06:05 -0600

    feat: :sparkles: Agregar rutas para la página de inicio y autenticación, incluyendo inicio de sesión y recuperación de contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'auth',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/reset',
+    component: ResetPasswordComponent,
+  },
+];

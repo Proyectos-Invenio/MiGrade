d2817728aee12bb08d642cc3cb1d4e1e7452c5d9
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-23 03:30:01 -0600

    feat: :sparkles: Agregar componente de visibilidad de menú por rol y servicio para obtener datos

---
+++ src/app/pages/visibilidad_menus/visibilidad-menu/visibilidad-menu.component.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule, Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { VisibilidadMenuService } from '../../../services/visibilidad_menu.service';
+import { IVisibilidadMenu } from '../../../interfaces/menu';
+
+@Component({
+  selector: 'app-visibilidad-menu',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, TableModule, InputTextModule, IconFieldModule, InputIconModule, TagModule],
+  templateUrl: './visibilidad-menu.component.html',
+  styleUrl: './visibilidad-menu.component.scss'
+})
+export class VisibilidadMenuComponent implements OnInit {
+  @ViewChild('dt') dt: Table | undefined;
+  public menu: IVisibilidadMenu[] = [];
+  public loading: boolean = true;
+  public selectedMenu: IVisibilidadMenu[] = [];
+
+  constructor(private visibilidadmenuService: VisibilidadMenuService, private _cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getMenu();
+  }
+
+  // Obtener administradores
+  getMenu(): void {
+    this.visibilidadmenuService.getMenu(0).subscribe({
+      next: (data) => {
+        this.menu = data;
+        this.loading = false;
+        this._cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error al cargar los administradores:', error);
+      },
+    });
+  }
+
+  // Aplicar filtro global
+  applyFilterGlobal(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dt?.filterGlobal(filterValue, 'contains');
+  }
+}
---
+++ src/app/services/visibilidad_menu.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environments';
+import { IVisibilidadMenu } from '../interfaces/menu';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VisibilidadMenuService {
+  private baseUrl = environment.baseUrl;
+  public identity: any;
+  public headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  constructor(private _http: HttpClient, private _router: Router) {}
+
+  getMenu(id: any): Observable<any> {
+    const token = localStorage.getItem('auth_token');
+    const headers = this.headers.set('Authorization', `Bearer ${token}`);
+    return this._http.get<IVisibilidadMenu>(
+      `${this.baseUrl}visibilidad_menu/${id}`,
+      {
+        headers,
+      }
+    );
+  }
+}

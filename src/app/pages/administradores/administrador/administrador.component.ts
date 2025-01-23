e7fa3b98a1ddc0c4d9d58cf965cd40a6e6b37112
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-22 23:21:56 -0600

    feat: :sparkles: Agregar componente para gestionar administradores

---
+++ src/app/pages/administradores/administrador/administrador.component.ts
@@ -0,0 +1,54 @@
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule, Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { AdministradoresService } from '../../../services/administrador.service';
+import { IAdministrador } from '../../../interfaces/usuarios';
+
+@Component({
+  selector: 'app-administrador',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, TableModule, InputTextModule, IconFieldModule, InputIconModule, TagModule],
+  templateUrl: './administrador.component.html',
+  styleUrl: './administrador.component.scss',
+})
+export class AdministradorComponent implements OnInit {
+  @ViewChild('dt') dt: Table | undefined;
+  public administradores: IAdministrador[] = [];
+  public loading: boolean = true;
+  public selectedAdministradores: IAdministrador[] = [];
+
+  constructor(private administradoresService: AdministradoresService, private _cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getAdministradores();
+  }
+
+  // Obtener administradores
+  getAdministradores(): void {
+    this.administradoresService.getAdministradores(0).subscribe({
+      next: (data) => {
+        this.administradores = data;
+        this.loading = false;
+        this._cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error al cargar los administradores:', error);
+      }
+    });
+  }
+
+  // Aplicar filtro global
+  applyFilterGlobal(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dt?.filterGlobal(filterValue, 'contains');
+  }
+
+
+
+}

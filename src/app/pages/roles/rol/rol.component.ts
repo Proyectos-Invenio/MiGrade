d9d6e6953d54f423873fdd20d9df0ad66c7c87dd
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-23 03:32:12 -0600

    feat: :sparkles: Agregar componente de gestión de roles

---
+++ src/app/pages/roles/rol/rol.component.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule, Table } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { RolesService } from '../../../services/roles.service';
+import { IRoles } from '../../../interfaces/auth';
+
+@Component({
+  selector: 'app-rol',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, TableModule, InputTextModule, IconFieldModule, InputIconModule, TagModule],
+  templateUrl: './rol.component.html',
+  styleUrl: './rol.component.scss'
+})
+export class RolComponent implements OnInit {
+  @ViewChild('dt') dt: Table | undefined;
+  public roles: IRoles[] = [];
+  public loading: boolean = true;
+  public selectedRoles: IRoles[] = [];
+
+  constructor(private rolesService: RolesService, private _cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.getRoles();
+  }
+
+  // Obtener administradores
+  getRoles(): void {
+    this.rolesService.getRoles(0).subscribe({
+      next: (data) => {
+        this.roles = data;
+        this.loading = false;
+        this._cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error al cargar los administradores:', error);
+      },
+    });
+  }
+
+  // Aplicar filtro global
+  applyFilterGlobal(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dt?.filterGlobal(filterValue, 'contains');
+  }
+}
---
+++ src/app/services/roles.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { environment } from '../environments/environments';
+import { IRoles } from '../interfaces/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RolesService {
+  private baseUrl = environment.baseUrl;
+  public identity: any;
+  public headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  constructor(private _http: HttpClient, private _router: Router) {}
+
+  getRoles(id: any): Observable<any> {
+    const token = localStorage.getItem('auth_token');
+    const headers = this.headers.set('Authorization', `Bearer ${token}`);
+    return this._http.get<IRoles>(
+      `${this.baseUrl}rol/${id}`,
+      { headers }
+    );
+  }
+}

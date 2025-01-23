b2dfa929c48c59078011f11680a5cd7ac979012d
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-23 03:31:34 -0600

    feat: :sparkles: Agregar componente de soporte técnico con su plantilla HTML

---
+++ src/app/pages/soporte-tecnico/soporte-tecnico.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soporte-tecnico',
+  standalone: true,
+  imports: [],
+  templateUrl: './soporte-tecnico.component.html',
+  styleUrl: './soporte-tecnico.component.scss'
+})
+export class SoporteTecnicoComponent {
+
+}

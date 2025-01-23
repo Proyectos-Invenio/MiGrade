12c7507979be50b7fab0edf19b36d1be930e5b5b
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-23 03:34:28 -0600

    feat: :sparkles: Agregar componente de inicio con mensaje de bienvenida y simplificar importaciones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from "../../components/sidebar/sidebar.component";
-import { FooterComponent } from "../../components/footer/footer.component";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, FooterComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

a212ae7f64a9d058649df95dbe598d647bd8d4c7
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-22 23:22:18 -0600

    feat: :sparkles: Agregar interfaces para gestionar estudiantes, administradores, profesores y padres

---
+++ src/app/interfaces/usuarios.ts
@@ -0,0 +1,40 @@
+export interface IEstudiante {
+  identification: number;
+  nombre: string;
+  clase_id: number;
+  padre: number;
+  informacion_contacto: number;
+  rol: number;
+  email: string;
+  password: string;
+  grado: number;
+  seccion: number;
+  sexo: string;
+}
+
+export interface IAdministrador {
+    identification: number;
+    nombre: string;
+    rol: number;
+    email: string;
+    password: string;
+}
+
+export interface IProfesor {
+    identification: number;
+    nombre: string;
+    asignatura_id: number;
+    rol: number;
+    email: string;
+    password: string;
+    informacion_contacto: number;
+}
+
+export interface IPadre {
+    identification: number;
+    nombre: string;
+    informacion_contacto: number;
+    rol: number;
+    email: string;
+    password: string;
+}

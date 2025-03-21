78bf917ca33e5fd12e771d72757a1d65e8380dfe
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:47:46 -0600

    feat: :sparkles: Agregar componente para editar administradores con plantilla básica

---
+++ src/app/pages/administradores/editar-administrador/editar-administrador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-administrador',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-administrador.component.html',
+  styleUrl: './editar-administrador.component.scss'
+})
+export class EditarAdministradorComponent {
+
+}

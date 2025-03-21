c34292381ec94541ee6a57f9c489434dbf735e8c
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:18 -0600

    feat: :sparkles: Agregar componente para editar encargados con plantilla básica

---
+++ src/app/pages/encargados/editar-encargado/editar-encargado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-encargado',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-encargado.component.html',
+  styleUrl: './editar-encargado.component.scss'
+})
+export class EditarEncargadoComponent {
+
+}

3d72aaf26657493945b6cd9188e5d86bb2dee21e
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:49:10 -0600

    feat: :sparkles: Agregar componente para editar visibilidad de menú con plantilla básica

---
+++ src/app/pages/visibilidad_menus/editar-visibilidad-menu/editar-visibilidad-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-visibilidad-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-visibilidad-menu.component.html',
+  styleUrl: './editar-visibilidad-menu.component.scss'
+})
+export class EditarVisibilidadMenuComponent {
+
+}

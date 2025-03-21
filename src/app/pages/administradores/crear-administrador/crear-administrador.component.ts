0b227c8b91709029e7b30cb4e432fe178008c9ec
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:47:37 -0600

    feat: :sparkles: Agregar componente para crear administradores con plantilla y estilos básicos

---
+++ src/app/pages/administradores/crear-administrador/crear-administrador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-administrador',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-administrador.component.html',
+  styleUrl: './crear-administrador.component.scss'
+})
+export class CrearAdministradorComponent {
+
+}

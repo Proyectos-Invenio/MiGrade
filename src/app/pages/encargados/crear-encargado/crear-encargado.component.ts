49a0ef49b8a1ff459af674c70640cb9a2a6bda4b
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:11 -0600

    feat: :sparkles: Agregar componente para crear encargados con plantilla básica

---
+++ src/app/pages/encargados/crear-encargado/crear-encargado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-encargado',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-encargado.component.html',
+  styleUrl: './crear-encargado.component.scss'
+})
+export class CrearEncargadoComponent {
+
+}

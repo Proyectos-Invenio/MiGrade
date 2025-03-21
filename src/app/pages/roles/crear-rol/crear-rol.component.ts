e1e3592fcf8266bc4c93deb4fe191b31d430af09
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:46 -0600

    feat: :sparkles: Agregar componente para crear roles con plantilla básica

---
+++ src/app/pages/roles/crear-rol/crear-rol.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-rol',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-rol.component.html',
+  styleUrl: './crear-rol.component.scss'
+})
+export class CrearRolComponent {
+
+}

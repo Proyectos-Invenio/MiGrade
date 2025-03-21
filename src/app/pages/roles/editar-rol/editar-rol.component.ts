6eb11f8e2a1b1fade56c11f196d4ff675dc7aa46
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:58 -0600

    feat: :sparkles: Agregar componente para editar roles con plantilla básica

---
+++ src/app/pages/roles/editar-rol/editar-rol.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-rol',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-rol.component.html',
+  styleUrl: './editar-rol.component.scss'
+})
+export class EditarRolComponent {
+
+}

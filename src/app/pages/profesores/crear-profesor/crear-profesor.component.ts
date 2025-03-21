e555e1b2b475106977c390338ce76b404c9fb290
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:30 -0600

    feat: :sparkles: Agregar componente para crear profesores con plantilla básica

---
+++ src/app/pages/profesores/crear-profesor/crear-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-profesor.component.html',
+  styleUrl: './crear-profesor.component.scss'
+})
+export class CrearProfesorComponent {
+
+}

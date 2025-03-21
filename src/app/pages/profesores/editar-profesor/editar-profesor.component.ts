023c975b713c5aa05e2574dd9d6a16f9fbfed382
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:48:38 -0600

    feat: :sparkles: Agregar componente para editar profesores con plantilla básica

---
+++ src/app/pages/profesores/editar-profesor/editar-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-profesor.component.html',
+  styleUrl: './editar-profesor.component.scss'
+})
+export class EditarProfesorComponent {
+
+}

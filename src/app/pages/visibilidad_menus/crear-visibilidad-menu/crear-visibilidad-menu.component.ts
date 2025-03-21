6395ce60fb68be018de02078008f0438c4505b4d
Emilio Rodríguez <emiliorb26@gmail.com>
2025-03-21 11:49:05 -0600

    feat: :sparkles: Agregar componente para crear visibilidad de menú con plantilla básica

---
+++ src/app/pages/visibilidad_menus/crear-visibilidad-menu/crear-visibilidad-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-visibilidad-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-visibilidad-menu.component.html',
+  styleUrl: './crear-visibilidad-menu.component.scss'
+})
+export class CrearVisibilidadMenuComponent {
+
+}
